1633fa91487287425054d6d9a2d4659f470e35ca
fischerlol <fischer-lochlan@iinet.net.au>
2022-07-12 03:35:48 +1000

    Remove TXID.zip and update batch script

---
+++ 2.bat
@@ -0,0 +1,217 @@
+@echo off
+cls
+
+set default_path=%cd%
+
+:main
+cls
+echo.
+echo ......................................................
+echo PRESS 1, 2, 3, 4, 5, 6 to select your task, or 7 to EXIT.
+echo ......................................................
+echo.
+echo 1 - Open MultiConverter (GUI)
+echo 2 - Convert PNG to BLP
+echo 3 - Run SQL queries and Headless Exporter
+echo 4 - Move the converted files to a mpq patch
+echo 5 - Run steps 1-4
+echo 6 - Remove exported files from wow.export folder
+echo 7 - EXIT
+echo.
+
+set /p m=Type 1, 2, 3, 4, 5, 6 or 7 then press ENTER:
+if %m%==1 goto multiconverter
+if %m%==2 goto png_to_blp
+if %m%==3 goto sql_queries
+if %m%==4 goto move_files
+if %m%==5 goto all
+if %m%==6 goto wow_export
+if %m%==7 goto eof
+
+:all
+goto multiconverter_all
+goto png_to_blp_all
+goto sql_queries_all
+goto move_files_all
+pause>nul|set/p = Steps 1-4 have finished running! Press any key to return to the menu ...
+goto main
+
+:multiconverter_all
+cls
+cd %default_path% & cd "Converting Tools\MultiConverter"
+echo Opening MultiConverter and accompanying folder...
+start "" "..\..\wow.export\"
+start /b /wait "" "MultiConverter_v0.0.14.exe"
+echo.
+
+:png_to_blp_all
+echo Moving files for conversion:
+echo.
+cd %default_path% 
+if exist "wow.export\interface\icons\*.png" (
+    xcopy /s /y /q "wow.export\*.png" "Converting Tools\BLP Converter\"
+) else (
+    echo .png files not present, moving on...
+)
+echo.
+echo Running png to blp converter...
+echo.
+if exist "Converting Tools\BLP Converter\interface\icons\*.png" (
+    cd "Converting Tools\BLP Converter\" & for /r %%i in (*.png) do BLPConverter.exe "%%i" & echo.
+) else (
+    echo .png files not present, moving on...
+)
+Echo Removing old files...
+cd %default_path% & echo.
+if exist "wow.export\interface\icons\*.png" (
+    cd "wow.export" & del /s *.png
+) else (
+    echo .png files not present, moving on...
+)
+echo.
+echo Moving converted files back to export folder...
+cd %default_path% & echo.
+if exist "Converting Tools\BLP Converter\interface\icons\*.blp" (
+    cd %default_path% & xcopy /s /y /q "Converting Tools\BLP Converter\*.blp" "wow.export"
+) else (
+    echo .blp files are not present, moving on...
+)
+echo.
+
+:sql_queries_all
+cd %default_path% & cd "SQL\"
+echo Running sql scripts...
+echo.
+mysql --login-path=local < items.sql acore_world_pvp && echo item_template - Command successfully executed
+mysql --login-path=local < item-dbc.sql dbc && echo item.dbc - Command successfully executed
+mysql --login-path=local < itemdisplayinfo-dbc.sql dbc && echo itemdisplayinfo.dbc - Command successfully executed
+echo.
+echo Running Headless Exporter...
+echo.
+cd %default_path% & cd "Converting Tools\Spell Editor\" & start /b /wait "" "HeadlessExport.exe"
+echo.
+
+:move_files_all
+cd %default_path% echo Moving .dbc files from WoW Spell Editor Export to patch-4.mpq in the wow folder
+xcopy /s /y /q "Converting Tools\Spell Editor\Export\*.dbc" "E:\World of Warcraft 3.3.5a\Data\patch-4.mpq\DBFilesClient"
+echo.
+echo Moving .m2 files from wow.export to patch-4.mpq in the wow folder
+xcopy /s /e /y /q "wow.export\*.m2" "E:\World of Warcraft 3.3.5a\Data\patch-4.mpq\"
+echo.
+echo Moving .blp files from wow.export to patch-4.mpq in the wow folder
+xcopy /s /e /y /q "wow.export\*.blp" "E:\World of Warcraft 3.3.5a\Data\patch-4.mpq\"
+echo.
+echo Moving .skin files from wow.export to patch-4.mpq in the wow folder
+xcopy /s /e /y /q "wow.export\*.skin" "E:\World of Warcraft 3.3.5a\Data\patch-4.mpq\"
+echo.
+pause>nul|set/p = Steps 1-4 finished! Press any key to return to the menu...
+goto main
+
+:multiconverter
+cls
+cd %default_path% & cd "Converting Tools\MultiConverter"
+echo Opening MultiConverter and accompanying folder...
+start "" "..\..\wow.export\"
+start /b /wait "" "MultiConverter_v0.0.14.exe"
+echo.
+pause>nul|set/p = MultiConverter finished! Press any key to return to the menu...
+goto main
+
+:png_to_blp
+cls
+echo Moving files for conversion:
+echo.
+cd %default_path% 
+if exist "wow.export\interface\icons\*.png" (
+    xcopy /s /y /q "wow.export\*.png" "Converting Tools\BLP Converter\"
+) else (
+    echo .png files not present, moving on...
+)
+echo.
+echo Running png to blp converter...
+echo.
+if exist "Converting Tools\BLP Converter\interface\icons\*.png" (
+    cd "Converting Tools\BLP Converter\" & for /r %%i in (*.png) do BLPConverter.exe "%%i" & echo.
+) else (
+    echo .png files not present, moving on...
+)
+Echo Removing old files...
+cd %default_path% & echo.
+if exist "wow.export\interface\icons\*.png" (
+    cd "wow.export" & del /s *.png
+) else (
+    echo .png files not present, moving on...
+)
+echo.
+echo Moving converted files back to export folder...
+cd %default_path% & echo.
+if exist "Converting Tools\BLP Converter\interface\icons\*.blp" (
+    cd %default_path% & xcopy /s /y /q "Converting Tools\BLP Converter\*.blp" "wow.export"
+) else (
+    echo .blp files are not present, moving on...
+)
+echo.
+pause>nul|set/p = BLPConverter finished! Press any key to return to the menu...
+goto main
+
+:sql_queries
+cls
+cd %default_path% & cd "SQL\"
+echo Running sql scripts...
+echo.
+mysql --login-path=local < items.sql acore_world_pvp && echo item_template - Command successfully executed
+mysql --login-path=local < item-dbc.sql dbc && echo item.dbc - Command successfully executed
+mysql --login-path=local < itemdisplayinfo-dbc.sql dbc && echo itemdisplayinfo.dbc - Command successfully executed
+echo.
+echo Running Headless Exporter...
+echo.
+cd %default_path% & cd "Converting Tools\Spell Editor\" & start /b /wait "" "HeadlessExport.exe"
+echo.
+pause>nul|set/p = SQL scripts and headless exporter finished! Press any key to return to the menu...
+goto main
+
+:move_files
+cls
+cd %default_path% echo Moving .dbc files from WoW Spell Editor Export to patch-4.mpq in the wow folder
+xcopy /s /y /q "Converting Tools\Spell Editor\Export\*.dbc" "E:\World of Warcraft 3.3.5a\Data\patch-4.mpq\DBFilesClient"
+echo.
+echo Moving .m2 files from wow.export to patch-4.mpq in the wow folder
+xcopy /s /e /y /q "wow.export\*.m2" "E:\World of Warcraft 3.3.5a\Data\patch-4.mpq\"
+echo.
+echo Moving .blp files from wow.export to patch-4.mpq in the wow folder
+xcopy /s /e /y /q "wow.export\*.blp" "E:\World of Warcraft 3.3.5a\Data\patch-4.mpq\"
+echo.
+echo Moving .skin files from wow.export to patch-4.mpq in the wow folder
+xcopy /s /e /y /q "wow.export\*.skin" "E:\World of Warcraft 3.3.5a\Data\patch-4.mpq\"
+echo.
+pause>nul|set/p = Files moved! Press any key to return to the menu...
+goto main
+
+:wow_export
+cls
+cd %default_path%
+echo Removing all files...
+echo.
+if exist "wow.export\.blp" (
+    del /s *.blp
+) else (
+    echo .blp files are not present, moving on...
+)
+if exist "wow.export\.png" (
+    del /s *.png, moving on...
+) else (
+    echo .png files are not present.
+)
+if exist "wow.export\.m2" (
+    del /s *.m2, moving on...
+) else (
+    echo .m2 files are not present.
+)
+if exist "wow.export\.skin" (
+    del /s *.skin, moving on...
+) else (
+    echo .skin files are not present.
+)
+echo.
+pause>nul|set/p = All files deleted! Press any key to return to the menu...
+goto main
--- SQL/item-dbc.sql
+++ SQL/item-dbc.sql
@@ -1,111 +1,29 @@
--- class
-set @class_weapon = 2;
-set @class_armor = 4;
-
--- subclass weapon
-set @subclass_axe_one_hand = 0;
-set @subclass_axe_two_hand = 1;
-set @subclass_bow = 2;
-set @subclass_gun = 3;
-set @subclass_mace_one_hand = 4;
-set @sublcass_mace_two_hand = 5;
-set @subclass_polearm = 6;
-set @subclass_sword_one_hand = 7;
-set @subclass_sword_two_hand = 8;
-set @subclass_staff = 10;
-set @subclass_fist_weapon = 13;
-set @subclass_dagger = 15;
-set @subclass_crossbow = 18;
-set @subclass_wand = 19;
-
--- sublcass armor
-set @subclass_miscellaneous = 0;
-set @subclass_cloth = 1;
-set @subclass_leather = 2;
-set @subclass_mail = 3;
-set @subclass_plate = 4;
-set @subclass_shield = 6;
-
--- inventorytype
-set @inventorytype_head = 1;
-set @inventorytype_shoulders = 3;
-set @inventorytype_shirt = 4;
-set @inventorytype_chest = 5;
-set @inventorytype_waist = 6;
-set @inventorytype_legs = 7;
-set @inventorytype_feet = 8;
-set @inventorytype_wrists = 9;
-set @inventorytype_hands = 10;
-set @inventorytype_one_hand = 13;
-set @inventorytype_shield = 14;
-set @inventorytype_bow = 15;
-set @inventorytype_back = 16;
-set @inventorytype_two_hand = 17;
-set @inventorytype_tabard = 19;
-set @inventorytype_robe = 20;
-set @inventorytype_main_hand = 21;
-set @inventorytype_off_hand = 22;
-set @inventorytype_holdable = 23;
-set @inventorytype_ranged = 26;
-
--- sheath 
-set @sheath_ranged_and_frill = 0;
-set @sheath_two_hand_weapon = 1;
-set @sheath_staff = 2;
-set @sheath_one_handed = 3;
-set @sheath_shield = 4;
-set @sheath_off_hand = 6;
-set @sheath_fist_weapon = 7;
-
--- material
-set @material_metal = 1;
-set @material_wood = 2;
-set @material_liquid = 3;
-set @material_chain = 5;
-set @material_plate = 6;
-set @material_cloth = 7;
-set @material_leather = 8;
-
--- other
-set @requiredlevel = 60;
-set @itemlevel = 60;
-set @flags_heroic = 8;
-set @flags_default = 0;
-set @quality_epic = 4;
-
-set @sound_override_subclassid = -1;
-
 -- https://www.wowhead.com/item=61326/vicious-gladiators-decapitator
-set @itemid = 61326;
-set @displayid = 81901;
-delete from item where itemid = @itemid;
+delete from item where itemid = 61326;
 insert into item(itemid, itemclass, itemsubclass, sound_override_subclassid, materialid, itemdisplayinfo, inventoryslotid, sheathid) 
-values (@itemid, @class_weapon, @subclass_axe_two_hand, @sound_override_subclassid, @material_metal, @displayid, @inventorytype_two_hand, @sheath_two_hand_weapon);
+select entry, class, subclass, soundoverridesubclass, material, displayid, inventorytype, sheath
+from acore_world_pvp.item_template where entry = 61326;
 
 -- https://www.wowhead.com/item=61327/vicious-gladiators-shanker
-set @itemid = 61327;
-set @displayid = 61327;
-delete from item where itemid = @itemid;
+delete from item where itemid = 61327;
 insert into item(itemid, itemclass, itemsubclass, sound_override_subclassid, materialid, itemdisplayinfo, inventoryslotid, sheathid) 
-values (@itemid, @class_weapon, @subclass_dagger, @sound_override_subclassid, @material_metal, @displayid, @inventorytype_one_hand, @sheath_one_handed);
+select entry, class, subclass, soundoverridesubclass, material, displayid, inventorytype, sheath
+from acore_world_pvp.item_template where entry = 61327;
 
 -- https://www.wowhead.com/item=61340/vicious-gladiators-pike
-set @itemid = 61340;
-set @displayid = 79850;
-delete from item where itemid = @itemid;
+delete from item where itemid = 61340;
 insert into item(itemid, itemclass, itemsubclass, sound_override_subclassid, materialid, itemdisplayinfo, inventoryslotid, sheathid) 
-values (@itemid, @class_weapon, @subclass_polearm, @sound_override_subclassid, @material_metal, @displayid, @inventorytype_two_hand, @sheath_two_hand_weapon);
+select entry, class, subclass, soundoverridesubclass, material, displayid, inventorytype, sheath
+from acore_world_pvp.item_template where entry = 61340;
 
 -- https://www.wowhead.com/item=61353/vicious-gladiators-longbow
-set @itemid = 61353;
-set @displayid = 61353;
-delete from item where itemid = @itemid;
+delete from item where itemid = 61353;
 insert into item(itemid, itemclass, itemsubclass, sound_override_subclassid, materialid, itemdisplayinfo, inventoryslotid, sheathid) 
-values (@itemid, @class_weapon, @subclass_bow, @sound_override_subclassid, @material_wood, @displayid, @inventorytype_bow, @sheath_ranged_and_frill);
+select entry, class, subclass, soundoverridesubclass, material, displayid, inventorytype, sheath
+from acore_world_pvp.item_template where entry = 61353;
 
 -- https://www.wowhead.com/item=61343/vicious-gladiators-staff
-set @itemid = 61343;
-set @displayid = 79845;
-delete from item where itemid = @itemid;
+delete from item where itemid = 61343;
 insert into item(itemid, itemclass, itemsubclass, sound_override_subclassid, materialid, itemdisplayinfo, inventoryslotid, sheathid) 
-values (@itemid, @class_weapon, @subclass_staff, @sound_override_subclassid, @material_metal, @displayid, @inventorytype_two_hand, @sheath_staff);
+select entry, class, subclass, soundoverridesubclass, material, displayid, inventorytype, sheath
+from acore_world_pvp.item_template where entry = 61343;
